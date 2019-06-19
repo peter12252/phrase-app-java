584dc37dfd76ff3b4ab94ab83d28b647649f55d3
Hubert Pan <hubert.pan@sharecare.com>
2019-06-19 18:32:43 -0400

    PDS-1919 Attempt github synch with phraseapp

--- translationApiExample.sh
+++ translationApiExample.sh
@@ -5,7 +5,7 @@ phrase_app_password="phrase"
 #list the PhraseApp projects
 curl -H "User-Agent: Sharecare Test App (hubert.pan@sharecare.com)" -u $phrase_app_user:$phrase_app_password "https://api.phraseapp.com/api/v2/projects"
 
-phrase_app_project_id="00000000000000004158e0858d2fa45c"
+phrase_app_project_id="ae71de126dd7f183e660a9bdef2e62ed"
 
 #list locales
 curl -H "User-Agent: Sharecare Test App (hubert.pan@sharecare.com)" -u $phrase_app_user:$phrase_app_password "https://api.phraseapp.com/api/v2/projects/$phrase_app_project_id/locales"
