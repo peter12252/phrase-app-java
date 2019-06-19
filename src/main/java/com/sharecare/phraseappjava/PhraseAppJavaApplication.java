c47958ff0243fc70cc53ab2fc53a1ead537ff353
Hubert Pan <hubert.pan@sharecare.com>
2019-06-19 10:33:25 -0400

    Initial commit

---
+++ src/main/java/com/sharecare/phraseappjava/PhraseAppJavaApplication.java
@@ -0,0 +1,13 @@
+package com.sharecare.phraseappjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhraseAppJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhraseAppJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sharecare/phraseappjava/PhraseAppJavaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sharecare.phraseappjava;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PhraseAppJavaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
